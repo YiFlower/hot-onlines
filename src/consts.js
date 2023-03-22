71c8ed1b71b9345e5abd5cf86175d9b6a4729f2b
y1 <cq.yh@qq.com>
2023-03-22 09:27:24 +0800

    修改部分变量信息

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Air Blog`;
-export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_TITLE = `Hot Online`;
+export const SITE_DESCRIPTION = 'Hot events in one hand';
+export const SITE_EMAIL = '446615324@qq.com'
+export const SITE_NAME = 'mv.onlines.asia';
+export const SITE_URL = "https://mv.onlines.asia";
